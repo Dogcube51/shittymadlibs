d6d24966e1f960c52f810372445da82d5509121b
Dogcube51 <54759705+Dogcube51@users.noreply.github.com>
2020-03-22 23:35:50 -0500

    the .py file and the exe

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+adjective = input("Type in a adjective: ")
+noun = input("Type in a noun: ")
+plural_noun = input("type in a plural noun: ")
+name = input("type a name: ")
+article_of_clothing = input("type in a article of clothing: ")
+City_name = input("type in the name of a city: ")
+plural_noun_two = input("type in a plural noun: ")
+adjective_two = input("type in an adjective: ")
+noun_two = input("type in a noun: ")
+adjective_three = input("type  inn a adjective: ")
+print("There are many " + adjective + " ways to choose a " + noun + " to read")
+print("First, you could ask for recommendations from your friends and " + plural_noun + ".")
+print(
+    "Just don't ask Aunt " + name + " she only reads" + adjective_three + " books with " + article_of_clothing + " and monsters on the cover")
+print(
+    "If your friends and family are no help try checking out the " + noun_two + " Review in the " + City_name + "times")
+print("If the" + plural_noun_two + " featured there are too " + adjective_two + " for your taste try something a little different")
